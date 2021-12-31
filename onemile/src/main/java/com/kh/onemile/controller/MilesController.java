5e4fd1aa80488a44c00585845936066173b34b00
ChoeByeol <cb68862643@gmail.com>
2021-12-31 11:12:54 +0900

    근우님이 주신 마일즈 컨트롤러

--- onemile/src/main/java/com/kh/onemile/controller/MilesController.java
+++ onemile/src/main/java/com/kh/onemile/controller/MilesController.java
@@ -5,42 +5,34 @@ import java.io.IOException;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import com.kh.onemile.service.image.ImageService;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.kh.onemile.service.miles.MilesService;
 import com.kh.onemile.vo.ImageVO;
 import com.kh.onemile.vo.MilesVO;
 
 @RequestMapping("/miles")
-@Controller
+@RestController
 public class MilesController {
-	
-	@Autowired
-	private MilesService milesService;
-	@Autowired @Qualifier("milesImage")
-	private ImageService imageService;
-	
-	//마일즈 생성
-	@GetMapping("/create")
-	public String create() {
-		return "miles/create";
-	}
-	@PostMapping("/create")
-	public String create(@ModelAttribute MilesVO milesVo,
-								@ModelAttribute ImageVO imageVo,
-		HttpSession session) throws IllegalStateException, IOException {
-		int writer =(int) session.getAttribute("logNo");
-		
-		milesVo.setMemberNo(writer);
-		milesService.create(milesVo);
-		if(imageVo!=null) {
-			imageService.regImage(imageVo);
-		}
-		return "redirect:/";
-	}
+    
+    @Autowired
+    private MilesService milesService;
+
+    //마일즈 생성
+    @GetMapping("/create")
+    public String create() {
+        return "miles/create";
+    }
+    @PostMapping("/create")
+    public String create(@ModelAttribute MilesVO milesVo,HttpSession session) throws IllegalStateException, IOException {
+        int writer =(int) session.getAttribute("logNo");
+        
+        milesService.create(milesVo);
+
+        return "redirect:/";
+    }
 }
