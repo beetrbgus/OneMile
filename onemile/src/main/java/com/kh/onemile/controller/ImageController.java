6575326836659ab1094d5ef18971c501bdf0b7f3
lostMS95 <rmsdn0415@naver.com>
2022-01-06 00:42:49 +0900

    이미지컨트롤러 구현중

--- onemile/src/main/java/com/kh/onemile/controller/ImageController.java
+++ onemile/src/main/java/com/kh/onemile/controller/ImageController.java
@@ -1,29 +1,26 @@
 package com.kh.onemile.controller;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URLEncoder;
-
-import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.kh.onemile.entity.image.ImageDTO;
-import com.kh.onemile.repository.reply.ReplyDao;
 import com.kh.onemile.service.image.ImageService;
-import com.kh.onemile.service.reply.ReplyService;
 import com.kh.onemile.util.SaveFile;
-import com.kh.onemile.vo.reply.ReplyVO;
+import com.kh.onemile.vo.ImageDownloadVO;
 
 @RestController
 @RequestMapping("/image")
@@ -32,24 +29,14 @@ public class ImageController {
 	private SaveFile savefile;
 	@Autowired
 	private ImageService imageService;
-	private final String rootpath="D:/upload";
+
 	@GetMapping("/download")
 	public ResponseEntity<ByteArrayResource> profileUpload(@RequestParam int imageNo, @RequestParam String folder) throws IOException {
-		System.out.println("ImageController    imageNo  "+imageNo);
-		System.out.println("ImageController    folder  "+folder);
-		String path = "";
-		switch(folder) {
+		//이미지 다운로드 서비스 
+		ImageDownloadVO imageDownloadVO = imageService.download(imageNo, folder);
 		
-		case "member": path = rootpath+"/member"; break;
-		case "miles": path = rootpath+"/miles"; break;
-		case "cobuy": path = rootpath+"/cobuy"; break;
-		case "commu": path = rootpath+"/commu"; break;
-		case "social": path = rootpath+"/social"; break;
-		}
-		ImageDTO imageDTO = imageService.getImage(imageNo);
-				
-		byte[] data = savefile.downImg(path, imageNo);
-		ByteArrayResource resource = new ByteArrayResource(data);
+		ByteArrayResource resource = new ByteArrayResource(imageDownloadVO.getData());
+		ImageDTO imageDTO= imageDownloadVO.getImageDTO();
 		
 		String encodeName = URLEncoder.encode(imageDTO.getUploadName(), "UTF-8");
 		encodeName = encodeName.replace("+", "%20");
@@ -61,4 +48,15 @@ public class ImageController {
 				.contentLength(imageDTO.getFileSize())
 				.body(resource);
 	}
+	//파일 업로드
+	@PostMapping("/upload")
+	public int data9(@RequestParam MultipartFile attach,@RequestParam String folder) throws IllegalStateException, IOException{
+		if(attach.isEmpty()) {
+			throw new FileNotFoundException("업로드된 파일이 존재하지 않습니다");
+		}
+		int imageNo= imageService.regImage(attach , folder);
+		
+		
+		return imageNo;
+		}
 }
--- onemile/src/main/java/com/kh/onemile/service/image/ImageService.java
+++ onemile/src/main/java/com/kh/onemile/service/image/ImageService.java
@@ -4,11 +4,16 @@ import java.io.IOException;
 import java.util.List;
 import org.springframework.web.multipart.MultipartFile;
 import com.kh.onemile.entity.image.ImageDTO;
+import com.kh.onemile.vo.ImageDownloadVO;
 
 public interface ImageService {
 	final String path="D:/upload"; 
-	List<Integer> regImage(List<MultipartFile> attach, String save) throws IllegalStateException, IOException;
+	int regImage(MultipartFile attach, String savePath) throws IllegalStateException, IOException;
 	void deleteImage(int imageNo,String save);
 	List<ImageDTO> listByBoardNo(int boardNo);
 	ImageDTO getImage(int imageNo);
+	
+	int save(ImageDTO imageDTO);
+	ImageDownloadVO download(int imageNo, String folder) throws IOException;
+
 }
